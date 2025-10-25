13772a66bbd5f84e0b79bff7cc05d6a6302af574
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-25 06:25:32 -0700

    Multiple Routing

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -1,33 +1,33 @@
 const express = require("express")
 const app = express()
 
-app.get("/ab*cd",(req,res)=>{
-    res.send("Routing advanced concept")
-})
-
-// app.get(/a/,(req,res)=>{
-//     res.send("This is advanced routing")
-// })
-
-app.get(/.*fly$/,(req,res)=>{
-    res.send("This is regex checking")
-})
-
-app.get("/user",(req,res)=>{
-    console.log(req.query)
-    res.send("Data submitted successfully")
-})
-
-app.get("/user/:userId",(req,res)=>{
-    console.log(req.params)
-    res.send("Data submitted successfully")
-})
-
-app.get("/user/:userId/:userName/:userPassword",(req,res)=>{
-    console.log(req.params)
-    res.send("Data finished because i don't know")
-})
+app.use("/",
+    [
+    (req,res,next)=>
+    {
+        console.log("1st route")
+        next()
+    },
+    (req,res,next)=>
+    {
+        console.log("2nd route")
+        // res.send("Data submitted by 2nd route")
+        next()
+    },
+    (req,res,next)=>
+    {
+        console.log("3rd route")
+        // res.send("Data submitted by 3rd route")
+        next()
+    },
+    (req,res,next)=>
+    {
+        console.log("4th route")
+        res.send("Data submitted by 4th route")
+    }
+    ]
+)
 
 app.listen(8081,()=>{
     console.log("Port Started Running at 8081")
-})
+});
