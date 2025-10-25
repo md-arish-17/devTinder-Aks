a7389f8f9503d14310b21b4cac41ebc5c5ee2828
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-25 01:45:25 -0700

    Advanced Routing Concept

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -1,16 +1,31 @@
 const express = require("express")
 const app = express()
 
+app.get("/ab*cd",(req,res)=>{
+    res.send("Routing advanced concept")
+})
+
+// app.get(/a/,(req,res)=>{
+//     res.send("This is advanced routing")
+// })
+
+app.get(/.*fly$/,(req,res)=>{
+    res.send("This is regex checking")
+})
+
 app.get("/user",(req,res)=>{
-    res.send("All data got fetched successfully")
+    console.log(req.query)
+    res.send("Data submitted successfully")
 })
 
-app.post("/user",(req,res)=>{
-    res.send("All data posted successfully")
+app.get("/user/:userId",(req,res)=>{
+    console.log(req.params)
+    res.send("Data submitted successfully")
 })
 
-app.delete("/user",(req,res)=>{
-    res.send("Data deleted successfully")
+app.get("/user/:userId/:userName/:userPassword",(req,res)=>{
+    console.log(req.params)
+    res.send("Data finished because i don't know")
 })
 
 app.listen(8081,()=>{
