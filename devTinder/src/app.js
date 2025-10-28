4d5dbfd663f2401c0461c9ae908ea5648db7f375
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-27 17:40:53 -0700

    Without Middleware

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -1,32 +1,29 @@
 const express = require("express")
 const app = express()
 
-app.use("/",
-    [
-    (req,res,next)=>
-    {
-        console.log("1st route")
-        next()
-    },
-    (req,res,next)=>
-    {
-        console.log("2nd route")
-        // res.send("Data submitted by 2nd route")
-        next()
-    },
-    (req,res,next)=>
-    {
-        console.log("3rd route")
-        // res.send("Data submitted by 3rd route")
-        next()
-    },
-    (req,res,next)=>
-    {
-        console.log("4th route")
-        res.send("Data submitted by 4th route")
+//Without middleware concept
+
+app.use("/getUserData",(req,res)=>{
+    const token = "abcd"
+    const isAuthorized = token === "abcd"
+    if(isAuthorized){
+        res.send("All data sent")
     }
-    ]
-)
+    else{
+        res.status(401).send("Unauthorised request")
+    }
+})
+
+app.delete("/deleteUser",(req,res)=>{
+    const token = "abcd"
+    const isAuthorized = token === "abcd"
+    if(isAuthorized){
+        res.send("User deleted")
+    }
+    else{
+        res.status(401).send("Unauthorised request")
+    }
+})
 
 app.listen(8081,()=>{
     console.log("Port Started Running at 8081")
