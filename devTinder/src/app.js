c3e0e4b36018b2c37c164acb0a78d3fa3d40cd7d
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-27 22:36:51 -0700

    Post API by Signup

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -1,6 +1,18 @@
 const express = require("express")
 const app = express()
 const {connectDB} = require("../src/config/database")
+const User = require("../src/models/user")
+
+app.post("/signup",async(req,res)=>{
+    const user = new User({
+        firstName:"Virat",
+        lastName:"Kohli",
+        emailId:"virat17@gmail.com",
+        password:"virat@123"
+    })
+    await user.save()
+    res.send("User added Successfully")
+})
 
 connectDB()
 .then(()=>{
